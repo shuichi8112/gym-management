28c0237d58f0066e0b19e0fd8aa5178d394b6cdc
shuichi8112 <shuichi8112@gmail.com>
2021-03-21 20:41:08 +0900

    新規投稿画面のview作成

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,23 @@
 class ClientsController < ApplicationController
   def index
+    @clients = Client.all
   end
   
   def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new
+    if @client.save
+      redirect to clients path
+    else
+      render :new
+    end
   end
 
   def show
+    
   end
 
   def edit
